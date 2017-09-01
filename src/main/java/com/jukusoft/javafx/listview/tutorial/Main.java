848d7be7e9ed446a788b16b20ef41ee8083d8169
JuKu <kuenzel.justin@t-online.de>
2017-09-01 17:35:00 +0200

    added main class.

---
+++ src/main/java/com/jukusoft/javafx/listview/tutorial/Main.java
@@ -0,0 +1,12 @@
+package com.jukusoft.javafx.listview.tutorial;
+
+/**
+ * Created by Justin on 01.09.2017.
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        //
+    }
+
+}
